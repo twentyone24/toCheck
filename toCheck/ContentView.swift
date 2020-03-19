cdacbffe8626f0f9d3a7c55243d441f32a456fdd
Navemics <navemics@gmail.com>
2020-03-19 09:44:32 +0530

    Initial Commit

---
+++ toCheck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  toCheck
+//
+//  Created by NAVEEN MADHAN on 3/19/20.
+//  Copyright © 2020 Navemics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
